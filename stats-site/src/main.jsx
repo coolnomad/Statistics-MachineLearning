6e359f5e5f2f87522d4de4ebd0f5bb8c90dedac5
coolnomad <dimuccidm@gmail.com>
2025-11-02 20:15:30 -0500

    wip local changes

--- stats-site/src/main.jsx
+++ stats-site/src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
+
+const basename = import.meta.env.VITE_BASE_URL || "/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Statistics-MachineLearning/stats-site/">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
